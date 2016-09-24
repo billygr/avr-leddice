dfec08f0a8776b40285e9ea62decedf80148d7e1
billygr <billygr@users.noreply.github.com>
2016-09-24 18:09:31 +0300

    Fix wake up from sleep INT0 issue

--- leddice.c
+++ leddice.c
@@ -49,22 +49,22 @@ SET_BIT(PORTD,6);SET_BIT(PORTD,5);SET_BIT(PORTD,4);_delay_ms(500);PORTD = 0x00;
 const int L_leds[6] = { 8, 16, 24, 48, 56, 112};
 const int R_leds[6] = { 8, 4, 12, 6, 14, 7};
 
-static int dice=0;
-
 ISR(INT0_vect)
 {
         /* Generate a random value for the LEDs
          * and display the value for 5 seconds
          */
         srand(TCNT1);
-        PORTD = L_leds[rand() % 6];
-        PORTB = R_leds[rand() % 6];
+        PORTD = PORTD | L_leds[rand() % 6];
+        PORTB = PORTB | R_leds[rand() % 6];
         _delay_ms(5000);
         PORTB=0x00;
         PORTD=0x00;
-                
+
+        // Make the INTO high to avoid triggering again
+        SET_BIT(PORTD,BUTTON_PIN);
         /* Debounce     */
-        loop_until_bit_is_clear(PIND,BUTTON_PIN);
+        //loop_until_bit_is_clear(PIND,BUTTON_PIN);
 }
 
 int main (void)
@@ -80,10 +80,10 @@ int main (void)
         PORTD = 0x00;
 
         /* PortD pin BUTTON_PIN is used for input      */
-        DDRD &= ~BUTTON_PIN;
+        //DDRD &= ~BUTTON_PIN;
 
         /* PortD pin enable the pull ups    */
-        PORTD |= BUTTON_PIN;
+        //PORTD |= BUTTON_PIN;
 
         /* Setup a timer running. We will use it for random seed        */
         TCCR1B |= (1 << CS10);
@@ -104,8 +104,14 @@ int main (void)
         PORTB = 0x00;
         PORTD = 0x00;
 
+        DDRD &= ~(1 << DDD2);     // Clear the PD2 pin
+        PORTD |= (1 << PORTD2);    // turn On the Pull-up
+
         /* interrupt on INT0 (button) on high level      */
-        MCUCR = (1<<ISC01) | (1<<ISC00);
+        //MCUCR = (1<<ISC01) | (1<<ISC00);
+        // Bug you cannot wake up from sleep with high
+
+        MCUCR =(0<<ISC01) | (0<<ISC00);
 
         /* Turn on interrupts   */
         GIMSK  |= (1<<INT0);
@@ -116,9 +122,9 @@ int main (void)
         /*      Main program loop       */
         while (1)
         {
-//              set_sleep_mode((_BV(SM0) | _BV(SM1)));
-//              sleep_enable();
-//              sleep_mode();
-//              sei();
+                set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+                sleep_enable();
+                sleep_cpu();
+                sleep_disable();
         }
 }
